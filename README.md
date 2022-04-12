c06b120f50e6aada656cde5353cd06cb807847fe
Shreya Ghate <ghate.s@northeastern.edu>
2022-04-11 20:06:15 -0400

    Add: serverless and handler code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # serverless
+serverless create --template aws-nodejs
+
---
+++ handler.js
@@ -0,0 +1,77 @@
+var aws = require("aws-sdk");
+const { profileEnd } = require("console");
+var ses = new aws.SES({ region: "us-east-1" });
+const dynamoDBTable = new aws.DynamoDB.DocumentClient({ region: "us-east-1" });
+const crypto = require("crypto");
+exports.SesSendEmail =  (event, context, callback) => {
+    let message = event.Records[0].Sns.Message;
+    let msgJSON = JSON.parse(message);
+    var eParams = {
+        Key: {
+            'id': msgJSON.Email
+        },
+        TableName: 'myDynamoDBTable'
+    };
+    dynamoDBTable.get(eParams, function (error, code) {
+        var codeJS = JSON.stringify(code);
+        if (error) {
+            console.log("Error",error);
+        }
+        else {
+            if (Object.keys(code).length >= 0) {
+                var flag = false;
+                if(code.Item == undefined){flag = true;}else
+                    if(code.Item.TimeToExist < (new Date).getTime()){flag = true;}
+                if(flag){
+                    var expirationTime = (new Date).getTime() + (60*1000*5);
+                    var params = {
+                        Item: {
+                            'id': msgJSON.Email,
+                            'token': crypto.randomBytes(16).toString("hex"),
+                            'TimeToExist': expirationTime
+                        },
+                        TableName: 'myDynamoDBTable'
+                    };
+
+                    dynamoDBTable.put(params, function (err, data) {
+                        if (err) {
+                            callback(err, null);
+                        } else {
+                            callback(null, data);
+                            let token = params.Item.token;
+                            let username = msgJSON.Email;
+                            console.log('username', username);
+                            console.log('token', token);
+                            var cParams = {
+                                Destination: {
+                                    ToAddresses: [username]
+                                },
+                                Message: {
+                                    Body: {
+                                        Text: {
+                                            Data: "http://prod.shreyaghate.me/v1/verifyUserEmail?email="+username+"&token="+token
+                                        }
+                                    },
+                                    Subject: {
+                                        Data: "Please click this link to verify your email address (Valid for 5 minutes)"
+                                    }
+                                },
+                                Source: "csye6225-spring22@prod.shreyaghate.me"
+                            };
+                            ses.sendEmail(cParams, function (err, data) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                else {
+                                    console.log("Email has been sent!");
+                                    
+                                }
+                            });
+                        }
+                    });
+                }else
+                console.log( code , "User exists");
+            }
+        }
+    });
+};
