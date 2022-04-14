aa5eff06adeff8a992375bb2fd5801259406fc07
Shreya Ghate <ghate.s@northeastern.edu>
2022-04-14 11:47:55 -0400

    Update: A9 latest changes

--- handler.js
+++ handler.js
@@ -55,7 +55,7 @@ exports.SesSendEmail =  (event, context, callback) => {
                                             Charset: "UTF-8",
                                             Data: `<h3>Hi ${username}!</h3><br/>
                                             <p>Please click on this link to verify your email address and be able to access more services. <b>Remember the link is valid for only 5 minutes</b></p><br/>
-                                            <a href="http://prod.shreyaghate.me/v1/verifyUserEmail?email=${username}&token=${token}">http://prod.shreyaghate.me/v1/verifyUserEmail?email=${username}&token=${token}</a>
+                                            <a href="http://prod.shreyaghate.me/v1/verifyUserEmail?email=${username}&token=${token}" "target="_blank">http://prod.shreyaghate.me/v1/verifyUserEmail?email=${username}&token=${token}</a>
                                             <p>Best,<br/>
                                             Team CSYE-6225 Prod, Shreya Ghate</p>`
                                         },
